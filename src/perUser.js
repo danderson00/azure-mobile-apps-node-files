b2df9ec5aa0891620b098602055da524e943db7f
Dale Anderson <Dale Anderson>
2016-07-12 18:43:14 -0700

    Refactored query predicate

--- src/perUser.js
+++ src/perUser.js
@@ -5,21 +5,28 @@ var errors = require('azure-mobile-apps/src/utilities/errors');
 
 module.exports = function (configuration) {
     return function (req, res, next) {
-        var tableDefinition = configuration.tables[req.params.tableName];
-        if(tableDefinition && tableDefinition.perUser) {
-            var context = req.azureMobile,
-                id = req.params.id,
-                userId = context.user && context.user.id,
-                table = context.tables(req.params.tableName);
+        var context = req.azureMobile,
+            tableName = req.params.tableName,
+            tableDefinition = configuration.tables[tableName];
 
-            table.where({ id: id }).read().then(function (items) {
-                if(items.length === 1 && userId === items[0][configuration.userIdColumn])
+        if(tableDefinition && tableDefinition.perUser) {
+            context.tables(tableName).where(queryPredicate()).read().then(function (items) {
+                if(items.length)
                     next();
                 else
                     next(errors.notFound());
             })
         } else {
             next();
+        }
+
+        function queryPredicate() {
+            var id = req.params.id,
+                userId = context.user && context.user.id,
+                predicate = { id: id };
+
+            predicate[configuration.userIdColumn] = userId;
+            return predicate;
         }
     };
 };
