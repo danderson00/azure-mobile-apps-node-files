f567f44409b37f738afff8a041822e2bc75e6b2d
Dale Anderson <Dale Anderson>
2016-07-14 14:20:45 -0700

    Updated e2etests

--- e2etest/app.js
+++ e2etest/app.js
@@ -3,8 +3,11 @@
 // ----------------------------------------------------------------------------
 
 var app = require('express')(),
-    mobileApp = require('azure-mobile-apps')()
+    mobileApp = require('azure-mobile-apps')({
+        auth: { azureSigningKey: "949799574C0A6422943C8C873819C03633740F49433BFC1FB5E65869F458E49F" }
+    })
 
 mobileApp.tables.add('DataEntity', { files: true });
+mobileApp.tables.add('PerUserDataEntity', { files: true, perUser: true });
 app.use(mobileApp);
 app.listen(3000);
